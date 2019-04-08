cbf633029c0626936eaca586c47d493994a8371c
Markus Konrad <markus.konrad@wzb.eu>
2019-04-08 14:50:07 +0200

    work in progr.

--- excercises/1_wzb_partners/wzb_partners_plot.R
+++ excercises/1_wzb_partners/wzb_partners_plot.R
@@ -1,9 +1,11 @@
 library(ggplot2)
+library(ggrepel)
+library(dplyr)
 library(maps)
 library(sf)
 
 # 1.
-partners <- read.csv('wzb_partners.csv', stringsAsFactors = FALSE)
+partners <- read.csv('wzb_partners_data.csv', stringsAsFactors = FALSE)
 
 # 2.
 worldmap_data <- st_as_sf(map('world', plot = FALSE, fill = TRUE))
@@ -12,11 +14,37 @@ worldmap_data <- worldmap_data[worldmap_data$ID != 'Antarctica',]   # 3.
 # 4.
 ggplot(partners) +                                     
     geom_sf(data = worldmap_data) +                    
-    geom_point(aes(x = lng, y = lat, color = country)) +    # TODO: country -> color ?
-    geom_label(aes(x = lng, y = lat, label = name),    
-               hjust = 0, vjust = 1, nudge_x = 3, size = 3) + 
+    geom_point(aes(x = institute_lon, y = institute_lat, color = type)) +
+    scale_color_discrete(guide = guide_legend(nrow = 4, title = NULL)) +
     theme_minimal() +
     theme(axis.title = element_blank(),
           axis.text = element_blank(),
           legend.position = 'bottom')
 
+# 5.
+partners_cities <- group_by(partners, city, city_lon, city_lat) %>% count()
+
+ggplot(partners_cities) +                                     
+    geom_sf(data = worldmap_data) +                    
+    geom_point(aes(x = city_lon, y = city_lat, size = n, color = city), alpha = 0.5) +
+    scale_color_discrete(guide = 'none') +
+    coord_sf(xlim = c(-30, 40), ylim = c(35, 65)) +
+    theme_minimal() +
+    theme(axis.title = element_blank(),
+          axis.text = element_blank(),
+          legend.position = 'bottom')
+
+
+
+
+# # 5.
+# ggplot(partners) +                                     
+#     geom_sf(data = worldmap_data) +                    
+#     geom_point(aes(x = institute_lon, y = institute_lat, color = type)) +
+#     geom_label_repel(aes(label = institution_short, x = institute_lon, y = institute_lat), size = 2) +
+#     scale_color_discrete(guide = guide_legend(nrow = 4, title = NULL)) +
+#     theme_minimal() +
+#     theme(axis.title = element_blank(),
+#           axis.text = element_blank(),
+#           legend.position = 'bottom')
+# 
